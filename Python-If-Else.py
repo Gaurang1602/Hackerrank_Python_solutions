eb8c90e8dd32cd70c48b866ea80d975657e75f97
Gaurang Agrawal <99584477+Gaurang1602@users.noreply.github.com>
2023-01-18 08:35:29 +0530

    Update Python-If-Else.py

--- Python-If-Else.py
+++ Python-If-Else.py
@@ -45,4 +45,12 @@ Explanation 1
  Solution-->>
  
  
- 
+ n = int(input().strip())
+check = {True: "Not Weird", False: "Weird"}
+
+print(check[
+        n%2==0 and (
+            n in range(2,6) or 
+            n > 20)
+    ])
+
