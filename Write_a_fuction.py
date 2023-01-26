9bb52a72ab9210a8933c3ebf30edfab1c1ab40f3
Gaurang Agrawal <99584477+Gaurang1602@users.noreply.github.com>
2023-01-26 09:19:09 +0530

    Update Write_a_fuction.py

--- Write_a_fuction.py
+++ Write_a_fuction.py
@@ -41,3 +41,20 @@ Explanation 0
 Solution-->>
 
 
+
+
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    if year % 400 == 0:
+        leap = True
+    elif year % 100 == 0:
+        leap = False
+    elif year % 4 == 0:
+        leap = True
+    
+    return leap
+year = int(input())
+print(is_leap(year))
+
