c542c43c13f823af77d5b6a6698752ce23a63bcf
Gaurang Agrawal <99584477+Gaurang1602@users.noreply.github.com>
2023-02-01 08:52:06 +0530

    Update ListComprehensions.py

--- ListComprehensions.py
+++ ListComprehensions.py
@@ -49,3 +49,12 @@ Sample Output 1
 
 Solution-->>
 
+if __name__ == '__main__':
+  
+    X = int(raw_input())
+    Y = int(raw_input())
+    Z = int(raw_input())
+    N = int(raw_input())
+
+ans = [[i, j, k] for i in range(X + 1) for j in range(Y + 1) for k in range(Z + 1) if i + j + k != N]
+print(ans)
